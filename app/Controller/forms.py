ffa7d218913b80218e700ac6c644e1ec719e9b35
Matthew_Cash <matthew@matthew-cash.com>
2023-10-17 13:13:50 -0700

    Add form and page for creating research positions

--- app/Controller/forms.py
+++ app/Controller/forms.py
@@ -1,5 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, PasswordField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import ValidationError, Length, DataRequired, Email, EqualTo
 from wtforms_sqlalchemy.fields import QuerySelectField
 from flask_login import current_user
+from app.Model.models import ResearchPosition
+
+class PositionForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(max=64)])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(max=1024)])
+    start_date = DateField('Start Date', validators=[DataRequired()])
+    end_date = DateField('End Date', validators=[DataRequired()])
+    time_commitment = StringField('Time Commitment', validators=[DataRequired()])
+    research_fields = StringField('Research Fields', validators=[DataRequired()])
+    languages = StringField('Programming Languages')
+    additional_requirements = StringField('Additional Requirements')
+    submit = SubmitField('Create')
--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -4,6 +4,7 @@ from app import db
 from flask_login import current_user, login_required
 from config import Config
 from app.Model.models import ResearchPosition, Student, Faculty, User
+from app.Controller.forms import PositionForm
 
 routes_blueprint = Blueprint("routes", __name__)
 routes_blueprint.template_folder = Config.TEMPLATES_FOLDER
@@ -14,3 +15,23 @@ routes_blueprint.template_folder = Config.TEMPLATES_FOLDER
 def index():
     positions = ResearchPosition.query.all()
     return render_template("index.html", positions=positions)
+
+@routes_blueprint.route('/createposition', methods=['GET', 'POST'])
+def createposition():
+    form = PositionForm()
+    if form.validate_on_submit():
+        position = ResearchPosition(
+            title=form.title.data,
+            description=form.description.data,
+            start_date=form.start_date.data,
+            end_date=form.end_date.data,
+            time_commitment=form.time_commitment.data,
+            research_fields=form.research_fields.data,
+            languages=form.languages.data,
+            additional_requirements=form.additional_requirements.data
+        )
+        db.session.add(position)
+        db.session.commit()
+        flash(f"Position '{form.title.data}' created!")
+        return redirect(url_for('routes.index'))
+    return render_template('createposition.html', title='Create Position', form=form)
