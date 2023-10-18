3071cf5ac76dcf69322ebcf686e17bbb893ab8b2
andrewEdson <aedson8175@hotmail.com>
2023-10-18 12:37:39 -0700

    Addition of .flaskenv file to set up data base before first request

--- researchPositionApp.py
+++ researchPositionApp.py
@@ -3,21 +3,21 @@ from flask_login import current_user
 from datetime import datetime
 from config import Config
 from app import create_app
+from app.Model.models import User, ResearchPosition, Student, Faculty
 
 app = create_app(Config)
 
 
 @app.before_request
 def initDB(*args, **kwargs):
-     if app.got_first_request:
-         db.create_all()
+    db.create_all()
 
 
-@app.before_request
-def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
+# @app.before_request
+# def before_request():
+#     if current_user.is_authenticated:
+#         current_user.last_seen = datetime.utcnow()
+#         db.session.commit()
 
 
 if __name__ == "__main__":
