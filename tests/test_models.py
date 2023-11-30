e5079a9b7762c78b34641d809ef6e8b2ddcd466c
Chance <bradfordchance@gmail.com>
2023-11-30 14:24:19 -0800

    fixed testing issue, now able to run unittests!

--- tests/test_models.py
+++ tests/test_models.py
@@ -32,16 +32,22 @@ class TestModels(unittest.TestCase):
     #------------- Model Tests -------------
     
     def test_User_student(self):
+        # s = Student(username='CB', email='c.b@wsu.edu', first_name='Chance', last_name='Bradford', phone_number='1234',
+        #              wsu_id='1234', major='Cpts', gpa='4.0', graduation_date='5/10/2025', user_type="Student")
         pass
-        # to-do iteration3
 
     def test_User_faculty(self):
+        # f = Faculty(username='SAA', email='S.AA@wsu.edu', first_name='Sakire', last_name='Arslan Ay', phone_number='1234',
+        #             wsu_id='1234', user_type="Faculty")
         pass
-        # to-do iteration3
 
+    # tests for correct password hashing of User class model
     def test_password_hashing(self):
-        pass
-        # to-do iteration3
+        s = User(username='AE', email='A.E@wsu.edu', first_name='Andrew', last_name='Edson',
+                  phone_number='1234', wsu_id='1234')
+        s.set_password('testpswrd')
+        self.assertFalse(s.check_password('1234'))
+        self.assertTrue(s.check_password('testpswrd'))
 
     def test_enroll_student(self):
         pass
