950f69c8efbe8b3e295035976083a0a8ca740313
Chance <bradfordchance@gmail.com>
2023-12-02 23:32:15 -0800

    added registration get and post request tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,6 +3,7 @@ import pytest
 from app import create_app, db
 from app.Model.models import User, Student, Faculty, ResearchPosition, Application, ProgrammingLanguage, Field
 from config import Config
+from datetime import datetime
 
 
 class TestConfig(Config):
@@ -24,14 +25,9 @@ def test_client():
     ctx.push()
 
     yield  testing_client
-    # this is where the testing happens!     
+    # this is where the testing happens    
     ctx.pop()
 
-# helper function to create users
-def new_user(uname, uemail,pswd):
-    user = User(username=uname, email=uemail)
-    user.set_password(pswd)
-    return user
 
 # Creates database and user and inserts the user into database
 @pytest.fixture
@@ -39,41 +35,86 @@ def init_database():
 
     db.create_all()
         
-    test_user = new_user(uname='Cb', uemail='c.b@wsu.edu',pswd='1234')
-    
-    db.session.add(test_user)
+    test_student = Student(username='CB', email='c.b@wsu.edu', first_name='Chance', last_name='Bradford', phone_number='1234',
+                     wsu_id='1234', major='Cpts', gpa='4.0', graduation_date=datetime.utcnow(), user_type="Student")
+    # test_student.set_password('1234')
+    db.session.add(test_student)
     db.session.commit()
 
-    yield  # this is where the testing happens!
+    test_faculty = Faculty(username='SAA', email='S.AA@wsu.edu', first_name='Sakire', last_name='Arslan Ay',
+                            phone_number='1234', wsu_id='1111', user_type="Faculty")
+    # test_student.set_password('1111')
+    db.session.add(test_faculty)
+    db.session.commit()
+
+    yield 
 
     db.drop_all()
 
 #---------------------------------------
 #------------- Route Tests -------------
 
-def test_register_page(test_client):
-    pass
-    # to-do iteration3
+# tests the '/student_register' page (GET) request response
+def test_student_register_page(test_client, init_database):
+    response = test_client.get('/student_register')
+    assert response.status_code == 200
+    assert b"Register" in response.data
 
-def test_student_registration(test_client,init_database):
-    pass
-    # to-do iteration3
 
-def test_faculty_registration(test_client,init_database):
-    pass
-    # to-do iteration3
+# tests the '/faculty_register' page (GET) request response
+def test_faculty_register_page(test_client, init_database):
+    response = test_client.get('/faculty_register')
+    assert response.status_code == 200
+    assert b"Register" in response.data
 
-def test_invalid_login(test_client,init_database):
-    pass
-    # to-do iteration3
 
-def test_student_loginlogout(request, test_client,init_database):
-    pass
-    # to-do iteration3
+# tests the '/student_registration' form (POST) request response
+def test_student_registration(test_client, init_database):
+    response = test_client.post('/student_register', 
+                          data=dict(username='CB', email='c.b@wsu.edu', first_name='Chance', last_name='Bradford', phone_number='1234',
+                                    wsu_id='1234', major='Cpts', gpa='4.0', graduation_date=datetime.utcnow(), user_type="Student"),
+                          follow_redirects = True)
+    assert response.status_code == 200
 
-def test_faculty_loginlogout(request, test_client,init_database):
+    s = db.session.query(User).filter(Student.username=='CB')
+    assert s.first().email == 'c.b@wsu.edu'
+    assert s.count() == 1
+    assert b"Student Registration" in response.data   
+
+
+# tests the '/faculty_registration' form (POST) request response
+def test_faculty_registration(test_client, init_database):
+    response = test_client.post('/faculty_register', 
+                          data=dict(username='SAA', email='S.AA@wsu.edu', first_name='Sakire', last_name='Arslan Ay',
+                                    phone_number='1234', wsu_id='1111', user_type="Faculty"),
+                          follow_redirects = True)
+    assert response.status_code == 200
+
+    s = db.session.query(User).filter(Faculty.first_name=='Sakire')
+    assert s.first().wsu_id == '1111'
+    assert s.count() == 1
+    assert b"Faculty Registration" in response.data  
+
+
+def test_invalid_login(test_client, init_database):
     pass
-    # to-do iteration3
+    
+
+def test_student_login(request, test_client, init_database):
+    pass
+
+
+def test_student_logout(request, test_client, init_database):
+    pass
+
+
+def test_faculty_login(request, test_client,init_database):
+    pass
+
+
+def test_faculty_logout(request, test_client,init_database):
+    pass 
+
 
 def test_createposition(request, test_client,init_database):
     pass
